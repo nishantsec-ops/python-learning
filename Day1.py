1da399565aef815b92177582b6158a0cf547131c
Nishant <nishant30033@gmail.com>
2026-07-01 20:04:46 +0530

    Add files via upload
    Day01-python

---
+++ Day1.py
@@ -0,0 +1,29 @@
+#day1 
+#introduction to python programming language......
+
+print("hello world")
+print("45+43")
+print(67+78)
+#comments .... . single
+#'''  multiple comments
+#multiple lines comments  - ctrl forward
+print("hiii")
+
+#print("easy programming language")
+#print("introduced in 1991 vs guido va rosum")
+#print("single comment # used ")
+#print("for multiple comments ... is used")
+#print("for multiple lines comments shortcut is used - ctrl forward")
+#print("python language is dynamic that can be be used on different os")
+#print("\n - backslash n multiple statement in single ")
+#print("multiple line define ''')
+print('''hii
+  this is python programming
+  programming language
+  guido van rosum''')
+ #shell >>>
+ #python.org
+ #python -- version
+ # pyhton is high level language
+ # python is used for automation
+ 
