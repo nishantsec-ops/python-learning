d27ab21095c840464e72f661591380e969710628
Nishant <nishant30033@gmail.com>
2026-07-04 00:31:36 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,134 @@
+# # c = True or False..............
+
+# is_present = False
+# print(is_present)
+
+# #Numeric  : int, float, complex
+# #Boolean : True or False 
+
+# # indexing 
+
+# x = "programming"
+
+# # 0 by default in string indexing starts from 0
+# print(x[0])
+
+
+# #input(0)......
+# x ="Nishant"
+
+# #Input
+# x = input("enter a num: ")
+# x = 78
+# x = "78"
+
+# x = input("enter name: ") #string..... by deafault
+# y = input ("enter a num: ")  #string
+# print(x+y)
+
+# #datatype... : numeric, text
+# x = input("enter name: ") #string..... by deafault
+# y = int(input("enter a num: ")) #string
+# print(x+y)
+
+# #int (),float(), complex()
+
+# # enter three number.... : avg
+
+# x = int(input("enter name: ")) #string..... by deafault
+# y = int(input("enter a num: "))
+# z = int(input("enter a num: ")) 
+# avg = (x+y+z)/3
+
+# print(f"Avg: {avg}")
+
+
+# #type casting 
+#  #int(), float(), complex(), str(), boolean
+
+# x = 89
+# print(float(x))
+# print(complex(x))
+
+
+
+# # for complex number
+
+# x = 79+8j
+
+# # print(int(x))
+# # # x = int(input("enter a number:"))  #87
+# # # for reverse that 87 number we can use str() function to conv
+
+# # # print(f"reverse of number: {str(x)[::-1]}")
+
+
+# # #String ..... : functionalit..........................
+
+# # # data types, define and use
+
+# # #mutliple data...........store
+
+# # #list, tuple , dictionary , set
+# # #list stored value in sequence and each values have indexing
+
+# # x=["a","b","c","hii",43,45,54,21,True,False]
+# # #   1   2   3    4    5  6  7 8    9    10
+
+# # print(x)
+# # print(x[2])
+
+# #slicing
+# # print(x[0:5])
+# # print(x[-1:-6:-1])
+# # print(x[4][0])
+
+
+# # x = [2, 4, "abc", ["ra","ca","pa"], "python", 90]
+# # print(x[3][4][0])
+
+# # x = [ ["ra","ca","pa",764], "python", 90]
+# # a = str(x[0][-1])
+# # print(a[1])
+
+
+
+# #list features.....: multiple values,
+# # multiple datatypes,
+# #duplicate values,
+# #indexing
+# #mutable : add,change, delete
+#  # []
+
+# ### Tuple features
+# #list features.....: multiple values,
+# # multiple datatypes,
+# #duplicate values,
+# #indexing
+# #immutable
+#  # ()
+# # x  = ("reema", "python",45,54,23,True,False)
+ 
+#  #Dictonary
+# # x = {"key":"value","key1":67,"key3":78}
+# # print(x)
+# # print(x[3])
+
+# # dic - keys are always unique 
+
+# # st1 = {"name": "abhishek",
+# #        "id" :1,
+# #        "course": "python",
+# #        "modules"}
+
+
+
+# # numeric can converts into other numeric types
+# # text can not be converted into numeric types
+
+
+# x = int(input("enter a number:"))  #87
+# # for reverse that 87 number we can use str() function to conv
+
+# print(f"reverse of number: {str(x)[::-1]}")
+
